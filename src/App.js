51b92c3fea16a2cfaf7a51d1a65fbe0354460ba4
Arpit patel <arpitpateln993@gmail.com>
2025-03-20 23:18:42 +0530

    animation is added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
- 
 import './App.css';
-import { Routes,Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './containers/home';
 import About from './containers/about';
 import Contact from './containers/contact';
 import Portfolio from './containers/portfolio';
 import Resume from './containers/resume';
-import Skills from './containers/skills'
+import Skills from './containers/skills';
 import Navbar from './components/navbar';
- function App() {
+import ParticlesComponent from './utils/particles';
+import { loadFull } from 'tsparticles';
+import Particle from './utils/particles';
+import Particles from 'react-tsparticles';
+
+function App() {
+
+  const handleInit=async(main)=>{
+         await loadFull(main);
+  }
   return (
     <div className="App">
-      {/*particles js*/}
-     <Navbar/>
-      {/*main page content */}
-
-
+      
+      <Navbar />
+      <Particle id="particles" options={Particles} init={handleInit} />
       <Routes>
-      <Route index path="/" element={<Home/>}></Route>
-      <Route  path="/about" element={<About/>}></Route>
-      <Route path="/contact" element={<Contact/>}></Route>
-      <Route  path="/portfolio" element={<Portfolio/>}></Route>
-      <Route path="/resume" element={<Resume/>}></Route>
-      <Route path="/skills" element={<Skills/>}></Route>
+        <Route index path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/skills" element={<Skills />} />
       </Routes>
-          </div>
+     
+    </div>
   );
 }
 
